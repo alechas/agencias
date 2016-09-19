8022cc265eb4154e29d2f3c943d02c1150f27496
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-19 19:00:31 -0300

    s

--- index.php
+++ index.php
@@ -8,10 +8,11 @@
 
 <form method="post" action="Nexo.php">
 <div class ="CajaInicio Animated bounceIn">
-<input type="text" name= "nombre" id = "nombre">
-<input type="text" name= "localidad" id = "localidad">
-<input type="text" name= "pr_noche" id = "pr_noche">
-</input>
+<lavel> Nombre del hotel </lavel>
+<input type="text" name= "nombre" id = "nombre"></input>
+<input type="text" name= "localidad" id = "localidad"></input>
+<input type="text" name= "pr_noche" id = "pr_noche"></input>
+
 <br> </br>
 <input class= "MiBotonUtn" type= "submit" value="Ingresar" name="ingresar" id="ingreso"> </input>
 <input class= "MiBotonUtn" type= "submit" value="Salir" name="salir" id="salida"> </input> 
