7901122bad9e2c7c721c984e1d905d1b017e8d1f
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-19 18:59:05 -0300

    creo index

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>Clase 05</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet" type="text/css" href="animacion.css">
+</head>
+<body>
+
+<form method="post" action="Nexo.php">
+<div class ="CajaInicio Animated bounceIn">
+<input type="text" name= "nombre" id = "nombre">
+<input type="text" name= "localidad" id = "localidad">
+<input type="text" name= "pr_noche" id = "pr_noche">
+</input>
+<br> </br>
+<input class= "MiBotonUtn" type= "submit" value="Ingresar" name="ingresar" id="ingreso"> </input>
+<input class= "MiBotonUtn" type= "submit" value="Salir" name="salir" id="salida"> </input> 
+
+	</div>
+</form>
+
+</body>
+</html>
